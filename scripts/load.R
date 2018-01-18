52de7d30b16540f4a3fb74498e7c60f8e21caf0f
Aaron Peikert <aaron.peikert@posteo.de>
2018-01-18 22:18:48 +0100

    init

---
+++ scripts/load.R
@@ -0,0 +1,2 @@
+#----load----
+movies <- read_csv(url("http://vincentarelbundock.github.io/Rdatasets/csv/ggplot2/movies.csv"))
---
+++ scripts/packages.R
@@ -0,0 +1,3 @@
+if(!require("pacman"))install.packages("pacman")
+pacman::p_load("tidyverse", "here")
+pacman::p_unload("pacman")
